93b75ff8d7bf66deb720a6d274ce8b07dd2a2a91
Pfleghaar, Scot <Scot.Pfleghaar@bestbuy.com>
2019-06-06 08:34:48 -0500

    Worker Process Setup

---
+++ worker/index.js
@@ -0,0 +1,19 @@
+const keys = require('./keys');
+const redis = require("redis");
+
+const redisClient = redis.createClient({
+    host: keys.redisHost,
+    port: keys.redisPort,
+    retry_strategy: () => 1000
+});
+const sub = redisClient.duplicate();
+
+// This is very slow - Intentional
+const fib = (index) => {
+    return index < 2 ? 1 : (fib(index - 1) + fib(index - 2));
+};
+
+sub.on('message', (channel, message) => {
+    redisClient.hset('values', message, fib(parseInt(message)))
+});
+sub.subscribe('insert');
