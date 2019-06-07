9891891a14c0448fadaf19c124786fa0b0adcc4f
Pfleghaar, Scot <Scot.Pfleghaar@bestbuy.com>
2019-06-07 07:50:04 -0500

    updates

--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -1,15 +1,11 @@
-import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-class OtherPage extends Component {
-    render() {
-        return (
-            <div>
-                Im some other page
-                <Link to="/">Go Back Home</Link>
-            </div>
-        );
-    }
-}
-
-export default OtherPage;
+export default () => {
+  return (
+    <div>
+      Im some other page
+      <Link to="/">Go back to home page!</Link>
+    </div>
+  );
+};
